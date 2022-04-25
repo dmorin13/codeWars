ebdacda0bb9085f603441f209337b56c5d4c4c5a
Daniela Morin <danielamorin@Danielas-MacBook-Pro.local>
2022-04-25 14:22:21 -0400

    My First CodeWar

---
+++ seatsInTheater.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/588417e576933b0ec9000045
+//create a function, that return the number of people's 
+//view you are going to block while exiting the theater.
+
+
+function seatsInTheater(nCols, nRows, col, row) {
+  //coding and coding..
+
+ return (nCols-(col -1))*(nRows-row)
+}
+
