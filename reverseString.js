a886fc15e2fecb386d6a18116b0819e10c857227
Daniela Morin <dmorin@wellesley.edu>
2022-05-16 09:09:16 -0400

    Add files via upload
    practice

---
+++ reverseString.js
@@ -0,0 +1,7 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow' 
+// reverse method only for arrays []
+
+const solution  = str => str.split('').reverse().join('')
