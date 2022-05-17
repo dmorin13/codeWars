a1b78e5455f7c85abd410a52d114a73d0ee96d2c
Daniela Morin <dmorin@wellesley.edu>
2022-05-17 18:27:17 -0400

    Add files via upload

---
+++ removeSpaces.js
@@ -0,0 +1,15 @@
+//Remove String Spaces
+//remove the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  let newString = x.replace(' ', '')
+  
+  for(let i=0; i <= x.length; i++){
+
+   newString = newString.replace(' ', '')
+    console.log(newString)
+ }
+  return newString
+   console.log(newString)
+} 
+ 
