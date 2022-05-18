d9c6af427988575f4e585db56e83a379c93000be
Daniela Morin <dmorin@wellesley.edu>
2022-05-18 17:24:25 -0400

    Add files via upload

---
+++ boolToWord.js
@@ -0,0 +1,15 @@
+//https://www.codewars.com/kata/53369039d7ab3ac506000467
+
+function boolToWord( bool ){
+  //conditional
+   // shorthand ternary 
+   return bool === true ? "Yes" : "No";
+   // longer version 
+ // if ( bool === true) {
+ // //   return "Yes"
+ // // }else {
+ // //     return "No"
+ // //   }
+ 
+ }
+ 
