474447572fe9750a7a0de68e5a3e09639471411d
Daniela Morin <dmorin@wellesley.edu>
2022-05-28 14:46:10 -0400

    Add files via upload

---
+++ highestLowest.js
@@ -0,0 +1,19 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+function highAndLow(numbers){
+  numbers = numbers.split(" ");
+  return Math.max.apply(null, numbers) + " " +  Math.min.apply(null, numbers)
+}
+
+//test
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Example tests", () => {
+  it("Test 1", () => {
+    assert.strictEqual(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"), "42 -9");
+  });
+  it("Test 2", () => {
+    assert.strictEqual(highAndLow("1 2 3"), "3 1");
+  });
+});
